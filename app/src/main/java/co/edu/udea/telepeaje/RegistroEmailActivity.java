943eb81e97633f7709ed8c3fa8c04acdc9c3078a
JhonBa26 <yonbabolivar@hotmail.com>
2017-03-21 15:43:21 -0500

    Actividad RegistroEmailActivity agregada

--- app/src/main/java/co/edu/udea/telepeaje/RegistroEmailActivity.java
+++ app/src/main/java/co/edu/udea/telepeaje/RegistroEmailActivity.java
@@ -1,7 +1,9 @@
 package co.edu.udea.telepeaje;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistroEmailActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RegistroEmailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_email);
+    }
+
+    public void openRegistroPassword(View view){
+        Intent intent = new Intent(this, RegistroPasswordActivity.class);
+        startActivity(intent);
     }
 }
