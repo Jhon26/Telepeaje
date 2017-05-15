832bf46375e641a82d762fc4fb26573c53dbf147
dantexx00 <dantexx00@gmail.com>
2017-05-15 18:34:28 -0500

    Algunas cosas

--- app/src/main/java/co/edu/udea/telepeaje/NavigationDrawerActivity.java
+++ app/src/main/java/co/edu/udea/telepeaje/NavigationDrawerActivity.java
@@ -3,23 +3,32 @@ package co.edu.udea.telepeaje;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class NavigationDrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener
         , MisAutosFragment.OnFragmentInteractionListener
         , InfoPersonalFragment.OnFragmentInteractionListener{
+
+    //Listener para la base de datos
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    TextView textViewCorreo, textViewNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,18 @@ public class NavigationDrawerActivity extends AppCompatActivity
         setContentView(R.layout.activity_navigation_drawer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+
+            //Cuando cambia la sesion (inicia o cierra)
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user==null){//Sesion cerrada
+                    Log.i("SESSION", "sesión cerrada");
+                }
+            }
+        };
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -36,6 +57,13 @@ public class NavigationDrawerActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        View view = navigationView.getHeaderView(0);
+        textViewCorreo = (TextView) view.findViewById(R.id.correoTextView);
+        textViewNombre = (TextView) view.findViewById(R.id.nombreTextView);
+
+        textViewCorreo.setText( FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        textViewNombre.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
 
         //Inicializo el primer fragment
         Fragment fragment = new MisAutosFragment();
@@ -115,4 +143,17 @@ public class NavigationDrawerActivity extends AppCompatActivity
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener!=null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }
 }
